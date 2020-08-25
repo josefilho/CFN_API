5261c9de2439c5248c3e003c4a9c7f9595f4f422
Jose Carlos Filho <iamjose.filho@gmail.com>
2020-08-25 00:18:49 -0300

    Fixing deployment issue

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ const NutricionistController = require('./controllers/NutricionistController');
 const routes = express.Router();
 
 routes.get('/findbysubscription/:subs', NutricionistController.index);
+
 routes.get('/',(req, res)=>{
     return res.status(200).send(
         'USE THE ROUTE /findbysubscription/(HERE THE SUBSCRIPTION IN NUMBERS) TO SEARCH BY SUBSCRIPTION IN CFN\n\nMORE ROUTES COMING SOON'
